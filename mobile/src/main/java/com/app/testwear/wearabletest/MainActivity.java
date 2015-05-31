3a1c6146c254f9b257a383de1f61b6bdd43d0195
Roman Butok <smb.roman@gmail.com>
2015-05-31 20:17:02 +0300

    Voice input for notifications

--- mobile/src/main/java/com/app/testwear/wearabletest/MainActivity.java
+++ mobile/src/main/java/com/app/testwear/wearabletest/MainActivity.java
@@ -10,8 +10,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -26,10 +29,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private final static String TAG = MainActivity.class.getSimpleName();
     private final static String COUNT_KEY = "COUNT";
+    public static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
 
     private GoogleApiClient googleApiClient;
 
     private int count;
+
+    private TextView notificationRsult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +47,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.notification_button).setOnClickListener(this);
         findViewById(R.id.notification_action_button).setOnClickListener(this);
         findViewById(R.id.notification_page_button).setOnClickListener(this);
+        findViewById(R.id.notification_voice).setOnClickListener(this);
         findViewById(R.id.send_data).setOnClickListener(this);
+        notificationRsult = (TextView) findViewById(R.id.notification_result);
 
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
@@ -66,6 +74,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 .addApi(Wearable.API)
                 .build();
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        notificationRsult.setText(getMessageText(intent));
     }
 
     @Override
@@ -96,6 +109,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 notification = getNotificationWithPage();
                 showNotification(notification);
                 break;
+            case R.id.notification_voice:
+                notification = getNotificationWithVoice();
+                showNotification(notification);
+                break;
             case R.id.send_data:
                 PutDataMapRequest dataMap = PutDataMapRequest.create("/count");
                 dataMap.getDataMap().putInt(COUNT_KEY, count++);
@@ -103,9 +120,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
                         .putDataItem(googleApiClient, request);
                 break;
-            default:
-                notification = getSimpleNotification();
         }
+    }
+
+    private CharSequence getMessageText(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(EXTRA_VOICE_REPLY);
+        }
+        return null;
     }
 
     private void showNotification(Notification notification) {
@@ -156,13 +179,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         NotificationCompat.Action action = new NotificationCompat.Action.Builder(android.R.drawable.ic_dialog_email, getString(R.string.send_mail), mailPendingIntent).build();
 
-        Bitmap aBigBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.roman);
+        Bitmap aBigBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_cast_light);
 
         NotificationCompat.Builder notificationBuilder =
                 getDefaultBuilder(getString(R.string.notification_action_title), getString(R.string.notification_text))
                         .addAction(android.R.drawable.ic_dialog_map,
                                 getString(R.string.ahow_map), mapPendingIntent)
-                        .extend(new NotificationCompat.WearableExtender().addAction(action))
+                        .extend(new NotificationCompat.WearableExtender()
+                                .addAction(action)
+                                .setGravity(Gravity.BOTTOM))
                         .setStyle(new NotificationCompat.BigPictureStyle()
                                 .bigPicture(aBigBitmap));
 
@@ -192,4 +217,34 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 .build();
 
     }
+
+    private Notification getNotificationWithVoice() {
+        String replyLabel = getResources().getString(R.string.reply_label);
+        String[] replyChoices = getResources().getStringArray(R.array.reply_choices);
+
+        RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
+                .setLabel(replyLabel)
+                .setChoices(replyChoices)
+                .build();
+
+// Create an intent for the reply action
+        Intent replyIntent = new Intent(this, MainActivity.class);
+        PendingIntent replyPendingIntent =
+                PendingIntent.getActivity(this, 0, replyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+// Create the reply action and add the remote input
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(android.R.drawable.ic_input_add,
+                        getString(R.string.reply_label), replyPendingIntent)
+                        .addRemoteInput(remoteInput)
+                        .build();
+
+// Build the notification and add the action via WearableExtender
+        return getDefaultBuilder("Notification with relu", "This is notification with reply")
+                        .extend(new NotificationCompat.WearableExtender().addAction(action))
+                        .build();
+    }
+
+
 }
